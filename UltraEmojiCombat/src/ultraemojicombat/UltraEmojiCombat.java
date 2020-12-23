3a28d3102e1085201d4135d419e374214e9775df
[Andrea González] <[andreavgonzalez7@gmail.com[>
2020-12-23 15:36:58 -0300

    antes da AGREGAÇÃO

--- UltraEmojiCombat/src/ultraemojicombat/UltraEmojiCombat.java
+++ UltraEmojiCombat/src/ultraemojicombat/UltraEmojiCombat.java
@@ -12,10 +12,10 @@ public class UltraEmojiCombat {
         l[4] = new Lutador("Ufocobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
         l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
 
-        l[5].apresentar();
-        l[2].status();
+        l[2].apresentar();
+        l[3].status();
         l[3].getCategoria();
-        l[1].ganharLuta();
+        l[3].ganharLuta();
 
     }
 
